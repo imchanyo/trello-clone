264f03d9bbb3438a0072e3edf2ce31ac9da1f84b
imchanyo <chanyoung3320@gmail.com>
2023-05-08 18:57:24 +0900

    dragabble reordering (react.memo)

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { theme } from "./theme";
+
 import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
